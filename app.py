bd97c90b5a72cefb986de03aad74505529307765
whitedevil019 <84139300+abhisek2-eng@users.noreply.github.com>
2022-10-02 10:37:13 +0545

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,10 @@
 print("Hello, World")
 #ok
 print('Hacktoberfest is the best')
+print("Hacktoberfest swag are the best")
+
+#Python f-strings to Print Hello World
+
+store1 = "Hello"
+store2 = "World"
+print(f"{store1} {store2} using f-strings")
